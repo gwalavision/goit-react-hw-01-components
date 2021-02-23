9b7dbd6cfdcb718e5a096efca2ee5b56c03627b6
Maksym Humeniuk <maizeproduction@gmail.com>
2021-02-23 19:58:30 +0200

    prettifying the code

--- src/components/randomColorFn.js
+++ src/components/randomColorFn.js
@@ -1,10 +1,8 @@
-import colors from '../json/colors.json'
-
+import colors from '../json/colors.json';
 
 const randomColor = () => {
-    return colors[Math.round(Math.random() * (Number(colors.length) + 1 - 1) + 1)].hex
-}
+  return colors[Math.round(Math.random() * (Number(colors.length) + 1 - 1) + 1)]
+    .hex;
+};
 
-
-export default randomColor
-
+export default randomColor;
